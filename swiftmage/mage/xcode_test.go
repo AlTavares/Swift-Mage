808cbe064313a046db5de875b36de8a48a8e3dca
Alexandre Mantovani Tavares <alexandre@live.in>
2018-06-04 16:50:18 -0300

    test: tests for xcodebuild common commands

--- swiftmage/mage/xcode_test.go
+++ swiftmage/mage/xcode_test.go
@@ -1,1 +1,83 @@
-package mage_test
+package mage
+
+import (
+	"fmt"
+	"testing"
+)
+
+func init() {
+	DryRun = true
+}
+
+func TestXCodeBuild_BuildForTesting(t *testing.T) {
+	xc := &XCodeBuild{}
+	xc.BuildForTesting("testplatform")
+	cmd := xc.BuildCommand()
+	expected := "xcodebuild -destination generic/platform=testplatform ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES build-for-testing"
+	assertEqual(t, cmd, expected)
+}
+
+func TestXCodeBuild_Archive(t *testing.T) {
+	xc := &XCodeBuild{}
+	xc.Archive("sdktest", "pathtest")
+	cmd := xc.BuildCommand()
+	expected := "xcodebuild -sdk sdktest -archivePath pathtest -configuration Release archive"
+	assertEqual(t, cmd, expected)
+}
+
+func TestXCodeBuild_ExportArchive(t *testing.T) {
+	xc := &XCodeBuild{}
+	xc.ExportArchive("archivepathtest", "exportpathtest", "exportoptionspathtest")
+	cmd := xc.BuildCommand()
+	expected := "xcodebuild -archivePath archivepathtest -exportPath exportpathtest -exportOptionsPlist exportoptionspathtest -allowProvisioningUpdates -exportArchive"
+	assertEqual(t, cmd, expected)
+}
+
+func TestXCodeBuild_TestWithoutBuilding(t *testing.T) {
+	xc := &XCodeBuild{}
+	xc.Test("destinationtest", "configurationtest", false)
+	cmd := xc.BuildCommand()
+	expected := "xcodebuild -destination destinationtest -configuration configurationtest test-without-building"
+	assertEqual(t, cmd, expected)
+}
+
+func TestXCodeBuild_TestBuilding(t *testing.T) {
+	xc := &XCodeBuild{}
+	xc.Test("destinationtest", "configurationtest", true)
+	cmd := xc.BuildCommand()
+	expected := "xcodebuild -destination destinationtest -configuration configurationtest test"
+	assertEqual(t, cmd, expected)
+}
+
+func TestXCodeBuild_Clean(t *testing.T) {
+	xc := &XCodeBuild{}
+	xc.Clean()
+	cmd := xc.BuildCommand()
+	expected := "xcodebuild clean -alltargets"
+	assertEqual(t, cmd, expected)
+}
+
+func TestXCodeBuild_Pretty(t *testing.T) {
+	xc := &XCodeBuild{}
+	xc.Pretty().Action("someaction")
+	cmd := xc.BuildCommand()
+	expected := "set -o pipefail && xcodebuild someaction | xcpretty -c"
+	assertEqual(t, cmd, expected)
+}
+
+func TestXCodeBuild_RandomArgs(t *testing.T) {
+	xc := &XCodeBuild{}
+	xc.AddKVArgument("-keyarg", "valuearg").
+		AddArgument("anotherargument")
+	cmd := xc.BuildCommand()
+	expected := "xcodebuild -keyarg valuearg anotherargument"
+	assertEqual(t, cmd, expected)
+}
+
+func assertEqual(t *testing.T, cmd string, expected string) {
+	if cmd != expected {
+		fmt.Println("Expected:", expected)
+		fmt.Println("Gotten:", cmd)
+		t.Fail()
+	}
+}

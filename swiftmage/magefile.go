9c7b77013c6d246a120ab14cb8b683141ec5ac72
Alexandre Mantovani Tavares <alexandre@live.in>
2018-06-07 16:20:44 -0300

    feat: compile mage when modified

--- swiftmage/magefile.go
+++ swiftmage/magefile.go
@@ -27,6 +27,10 @@ var (
 	xCodeBuildWorkspace = xcode.NewXCodeBuildWithWorkspace(Workspace, Scheme)
 )
 
+func init() {
+	Compile()
+}
+
 // Install all the dependencies
 func Bootstrap() {
 	mg.Deps(initEnvironment)
@@ -172,6 +176,11 @@ func Release() {
 }
 
 func Compile() {
+	modified, err := target.Path("swiftmage", "magefile.go", "tests.go")
+	if !modified && err == nil {
+		return
+	}
+	logger.Log("Updating binary...")
 	sh.Run("mage -compile swiftmage")
 }
 
